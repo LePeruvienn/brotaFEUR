fad86c7307135427fb089fcba37fdc641e6620e4
LePeruvienn <koto91@free.fr>
2025-03-31 18:19:41 +0200

    GREEN CIRCLE MOVING

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+	auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+	window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
+	sf::CircleShape circle(100.f);
+	circle.setFillColor(sf::Color::Green);
 
-        window.clear();
-        window.display();
-    }
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+			{
+				window.close();
+			}
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+		{
+			// left key is pressed: move our character
+			circle.move({-1.f, 0.f});
+		}
+
+		window.clear();
+		window.draw(circle);
+		window.display();
+	}
 }
